acedbb1b4e2042a3a459ce2192530e4b162f2557
Chandler Barnes <chandler.barnes96@gmail.com>
2018-04-15 11:16:27 -0500

    Added Student and Building agent, Campus Model

---
+++ congestion.py
@@ -0,0 +1,48 @@
+import random
+
+from mesa import Agent, Model
+from mesa.time import SimultaneousActivation
+from mesa.space import ContinuousSpace
+
+class StudentAgent(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        print(self.unique_id)
+        self.move()
+
+    def advance(self):
+        pass
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False)
+        new_position = random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+class BuildingAgent(Agent):
+    def __init__(self, name, model, x, y):
+        super().__init__(name, model, x, y)
+
+    def step(self):
+        print(self.name)
+
+class CampusModel(Model):
+    def __init__(self, N, width, height):
+        super().__init__(N, width, height)
+        self.num_agents = N
+        self.grid = ContinuousSpace(width, height, False)
+        self.schedule = SimultaneousActivation(self)
+        for i in range(self.num_agents):
+            a = StudentAgent(i, self)
+            self.schedule.add(a)
+
+            x = random.randrange(self.grid.width)
+            y = random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+    def step(self):
+        self.schedule.step()
---
+++ run.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+from congestion import *
+
+n_students = 12
+minutes = 60
+width = 5
+height = 5
+
+# create model
+model = CampusModel(n_students, width, height)
+
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for cell in model.grid.coord_iter():
+    cell_content, x, y = cell
+    agent_count = len(cell_content)
+    agent_counts[x][y] = agent_count
+plt.imshow(agent_counts, interpolation='nearest')
+plt.colorbar()
+plt.show()
+
+for i in range(minutes):
+    model.step()
+    time.sleep(10)
